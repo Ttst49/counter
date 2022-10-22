af5e9dbadd38659846b9914d792fda2a61e5c54b
Ttst49 <thibautstachnick@gmail.com>
2022-10-22 11:59:28 +0200

    initial commit

---
+++ javascript/main.js
@@ -0,0 +1,22 @@
+const boutonIncrease = document.querySelector('.increase')
+const boutonDecrease = document.querySelector('.decrease')
+const boutonReset = document.querySelector('.reset')
+const counter = document.querySelector('.number')
+
+let nombre= counter.innerHTML
+
+boutonIncrease.addEventListener('click',()=>{
+    console.log("coucou")
+    nombre +=1
+    counter.innerHTML = nombre
+})
+
+boutonDecrease.addEventListener('click',()=>{
+    nombre -=1
+    counter.innerHTML = nombre
+})
+
+boutonReset.addEventListener('click',()=>{
+    nombre =0
+    counter.innerHTML= nombre
+})
